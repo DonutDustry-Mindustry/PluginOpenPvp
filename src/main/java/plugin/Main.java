02b54edae965208bd7337f5cf61897187e751cac
Волшебник <127457914+VikoMin@users.noreply.github.com>
2025-07-13 05:50:42 +0700

    Fix /spectate

--- src/main/java/plugin/Main.java
+++ src/main/java/plugin/Main.java
@@ -21,7 +21,7 @@ public class Main extends Plugin {
         Log.info("Loaded openpvp plugin v@", Vars.mods.getMod("openpvp").meta.version);
         Log.info("Вы можете менять правила, но правила используемые плагином все равно будут назначены им!");
         Timer.schedule(()->{
-            if(GameOverWhen != null && GameStartWhen != 0){
+            if (GameOverWhen != null && GameStartWhen != 0){
                 long now = System.currentTimeMillis();
                 long elapsedMillis = now - GameStartWhen;
                 long totalGameMillis = 180 * 60 * 1000L;
@@ -47,45 +47,48 @@ public class Main extends Plugin {
                 if (Vars.world.tile((int)(player.x / 8f), (int)(player.y / 8f)).build.team == player.team())
                     Vars.world.tile((int)(player.x / 8f), (int)(player.y / 8f)).setNet(Blocks.air);
         });
-        handler.<Player>register("teams", "Посмотреть занятые команды.", (args, player) -> playerTeams.each(t->player.sendMessage(t.getTeam().coloredName())));
-        handler.<Player>register("spectate", "Перейти в серую команду.", (args, player) ->{
-            if(player.team() != Team.derelict) {
-                playerTeams.remove(playerTeams.find(SVOGOYDA -> SVOGOYDA.getTeam() == player.team()));
-                if(Groups.player.size() == 2)
+        handler.<Player>register("teams", "Посмотреть занятые команды.", (args, player) -> {
+            playerTeams.each(t->player.sendMessage(t.getTeam().coloredName()));
+        });
+        handler.<Player>register("spectate", "Перейти в серую команду.", (args, player) -> {
+            if (player.team() != Team.derelict) {
+                playerTeams.remove(playerTeams.find(p -> p.getTeam() == player.team()));
+                if (Groups.player.size() == 2)
                     gameStarted = false;
                 player.unit().kill();
                 Groups.build.each(b -> {
-                    if (b.team == player.team())
+                    if (b.team == player.team() && isOwner(player, player.team())
                         b.kill();
                 });
                 player.team(Team.derelict);
+                player.sendMessage("[green]Вы переведены в серую команду");
             } else {
                 player.sendMessage("[scarlet]Вы уже в серой команде!");
             }
         });
         handler.<Player>register("yes", "<#player-name>", "Одобрить запрос на вступление", (args, player)->{
             Player requester = Groups.player.find(p->p.plainName().contains(args[0]));
-            if(requester == null) {
+            if (requester == null) {
                 player.sendMessage("[scarlet]Игрок не найден");
                 return;
             }
-            if(requester.team() != Team.derelict) {
-                player.sendMessage("Извините, игрок вступил в команду "+requester.team().coloredName());
+            if (requester.team() != Team.derelict) {
+                player.sendMessage("Извините, игрок вступил в команду " + requester.team().coloredName());
                 return;
             }
             joinRequest samreq = joinRequests.find(p->p.getRequester()==requester);
-            if(player.team() != samreq.getTeam()) {
+            if (player.team() != samreq.getTeam()) {
                 player.sendMessage("[scarlet]Запрос отправлен не вашей команде!");
                 return;
             }
-            if(!isOwner(player, player.team())) {
+            if (!isOwner(player, player.team())) {
                 player.sendMessage("[scarlet]Вы не являетесь владельцем команды!");
                 return;
             }
             requester.team(player.team());
             joinRequests.remove(samreq);
             player.sendMessage("[green]Запрос одобрен!");
-            requester.sendMessage("[green]Владелец команды"+samreq.getTeam()+" одобрил запрос!");
+            requester.sendMessage("[green]Владелец команды" + samreq.getTeam() + " одобрил запрос!");
             awaitingClick.remove(requester);
         });
     }
@@ -94,8 +97,8 @@ public class Main extends Plugin {
     public void registerServerCommands(CommandHandler handler) {
         handler.<Player>register("teams", "Посмотреть занятые команды.", (args, player) -> playerTeams.each(t->Log.info(t.getTeam().coloredName())));
         handler.register("clicks-add", "<uuid>", "добавить игрока в ожидание клика", (args) -> {
-            Player fp = Groups.player.find(meow->meow.uuid().equals(args[0]));
-            if(fp != null && !awaitingClick.contains(fp)) {
+            Player fp = Groups.player.find(p->p.uuid().equals(args[0]));
+            if (fp != null && !awaitingClick.contains(fp)) {
                 awaitingClick.add(fp);
                 Log.info("Игрок добавлен в список!");
                 return;
@@ -103,7 +106,7 @@ public class Main extends Plugin {
             Log.info("Игрок не найден или он уже имеется в списке, посмотрите clicks!");
         });
         handler.register("clicks", "добавить игрока в ожидание клика", (args) -> {
-            awaitingClick.each(nyah->Log.info("@ @", nyah.plainName(), nyah.uuid()));
+            awaitingClick.each(l->Log.info("@ @", l.plainName(), p.uuid()));
         });
     }
 
